d5fbabcf8451e5055952958e3fd67b540a768969
samuelakerman <samsk66@gmail.com>
2017-03-14 22:17:56 -0400

    user model working

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	validates :name, presence: true, length: { minimum: 2, maximum: 20 }
+	validates :last_name, presence: true, length: { minimum: 2, maximum: 20 }
+	validates :username, presence: true, length: { minimum: 2, maximum: 20 }
+  	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  	has_secure_password
+end
