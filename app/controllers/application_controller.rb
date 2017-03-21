91d2a35726bcf97479bb0838c3de4f75d7bf255e
samuelakerman <samsk66@gmail.com>
2017-03-21 05:15:29 -0400

    updated home page with enrolled classes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,14 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   def home
+  	if !current_user.nil?
+  	 enrolled_ids = User.find_by(id: current_user.id).enrollments
+	 @enrolled_courses = []
+	  	enrolled_ids.each_with_index  do |i, index|
+	  	
+	  		@enrolled_courses[index] =  Course.find_by(id: i.course_id).name 
+		end
+	end
   end
   def not_found
   end
